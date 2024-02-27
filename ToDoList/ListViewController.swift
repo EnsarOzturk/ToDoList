62b672a3f07788d7668e68e8761ca98f20cb733b
Ensar Öztürk <ozturkkensar@gmail.com>
2024-02-27 14:58:35 +0300

    NavigationBar buttons color change has been made

--- ToDoList/ListViewController.swift
+++ ToDoList/ListViewController.swift
@@ -10,12 +10,33 @@ import UIKit
 class ListViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
+    var list = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        title = "ToDoList"
+        
+        title = "TO DO LIST"
+        self.navigationController?.navigationBar.tintColor = .black
     }
+}
 
+extension ListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
 
+extension ListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = list[indexPath.row]
+        return cell
+    }
+    
+    
 }
