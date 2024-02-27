86ced6874b972d5d2a59dcab7733873a15ba7354
Ensar Öztürk <ozturkkensar@gmail.com>
2024-02-27 10:19:55 +0300

    NavigationController title added

---
+++ ToDoList/ListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ListViewController.swift
+//  ToDoList
+//
+//  Created by Ensar on 27.02.2024.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "ToDoList"
+    }
+
+
+}
