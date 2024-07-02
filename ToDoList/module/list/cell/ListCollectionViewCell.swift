98d54773ebb0ef2b1581f213a9343a7359448a1d
Ensar Öztürk <ozturkkensar@gmail.com>
2024-07-02 13:14:38 +0300

    Created ListCollectionViewCell

---
+++ ToDoList/module/list/cell/ListCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ListCollectionViewCell.swift
+//  ToDoList
+//
+//  Created by Ensar on 2.07.2024.
+//
+
+import UIKit
+
+class ListCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
