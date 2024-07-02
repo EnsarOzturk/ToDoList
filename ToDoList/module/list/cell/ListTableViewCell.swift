45bd1c5596c13760ab81de5df60db6e8771abb71
Ensar Öztürk <ozturkkensar@gmail.com>
2024-07-02 13:28:55 +0300

    Created ListTableViewCell

---
+++ ToDoList/module/list/cell/ListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListTableViewCell.swift
+//  ToDoList
+//
+//  Created by Ensar on 2.07.2024.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+    @IBOutlet var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
